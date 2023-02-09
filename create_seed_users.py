3386e0850cdb61ca464c2b64056bd49346debf3a
Santiago Donoso <sand@kea.dk>
2023-02-09 15:38:34 +0100

    ...

--- create_seed_users.py
+++ create_seed_users.py
@@ -6,10 +6,18 @@ db.executescript(
   """
   BEGIN;
   DROP TABLE IF EXISTS users;
+  CREATE TABLE users(
+    id            TEXT,
+    name          TEXT,
+    email         TEXT,
+    PRIMARY KEY(id)
+  ) WITHOUT ROWID;
   COMMIT;
   """
 )
 
+db.execute("INSERT INTO users(id,name,email) VALUES('1','a','aa')")
+db.commit()
 
 
 
