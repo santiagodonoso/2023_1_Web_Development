d0c9255e5f2a8817570a8945c289c2b32bf305bc
Santiago Donoso <sand@kea.dk>
2023-02-13 09:31:24 +0100

    ...

--- data/create_table_users.sql
+++ data/create_table_users.sql
@@ -1,9 +1,13 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-  id        TEXT,
-  name      TEXT,
-  last_name TEXT,
+  id                TEXT,
+  username          TEXT,
+  name              TEXT,
+  last_name         TEXT,
+  total_followers   TEXT,
+  total_following   TEXT,
+  total_tweets      TEXT,
   PRIMARY KEY(id)
   ) WITHOUT ROWID;
 
