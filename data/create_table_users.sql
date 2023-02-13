7d63e9fb579c3a4ca4562d43a73d4c2c12648f01
Santiago Donoso <sand@kea.dk>
2023-02-13 09:53:09 +0100

    ...

--- data/create_table_users.sql
+++ data/create_table_users.sql
@@ -8,10 +8,13 @@ CREATE TABLE users(
   total_followers   TEXT,
   total_following   TEXT,
   total_tweets      TEXT,
+  avatar            TEXT,
   PRIMARY KEY(id)
   ) WITHOUT ROWID;
 
-INSERT INTO users VALUES("1","elonmusk","Elon", "Musk", "128900000", "177", "22700");
+INSERT INTO users VALUES("51602a9f7d82472b90ed1091248f6cb1","elonmusk","Elon", "Musk", "128900000", "177", "22700","51602a9f7d82472b90ed1091248f6cb1.jpg");
+INSERT INTO users VALUES("6268331d012247539998d7664bd05cc1","shakira","Shakira", "", "53700000", "235", "7999", "6268331d012247539998d7664bd05cc1.jpg");
+INSERT INTO users VALUES("a22da1effb3d4f03a0f77f9aa8320203","Rihanna","Rihanna", "", "107000000", "980", "10600", "a22da1effb3d4f03a0f77f9aa8320203.jpg");
 
 
 
