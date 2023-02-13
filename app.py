4846b4151c6dfa94c646f418a678f62a4bca7952
Santiago Donoso <sand@kea.dk>
2023-02-13 09:05:35 +0100

    ...

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from bottle import get, run
+from bottle import get, run, view
 
 ##############################
 @get("/<username>")
+@view("profile")
 def _(username):
-  return username
+  return 
 
 ##############################
 run(host="127.0.0.1", port=80, debug=True, reloader=True)
