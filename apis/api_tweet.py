577f851e31b2c4a09244492aa806f1e6cc10d314
Santiago Donoso <sand@kea.dk>
2023-02-27 09:54:00 +0100

    ...

--- apis/api_tweet.py
+++ apis/api_tweet.py
@@ -1,12 +1,20 @@
 from bottle import post, request, response
 import x
-
-
+import uuid
+import time
 
 @post("/tweet")
 def _():
   try: # SUCCESS
     x.validate_tweet()
+    db = x.db()
+    tweet_id = str(uuid.uuid4())
+    tweet_message = request.forms.get("message")
+    tweet_image = ""
+    tweet_created_at = time.time()
+    tweet_user_fk = "ebb0d9d74d6c4825b3e1a1bcd73ff49a"
+    db.execute("INSERT INTO tweets VALUES(?, ?, ?, ?, ?)", 
+               (tweet_id, tweet_message, tweet_image, tweet_created_at, tweet_user_fk))
     return "ok"
   except Exception as ex: # SOMETHING IS WRONG
     response.status = 400
