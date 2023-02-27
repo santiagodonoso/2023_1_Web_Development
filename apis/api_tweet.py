f7442e3db979c04300ee0a3857c6e5a3f0f06ccc
Santiago Donoso <sand@kea.dk>
2023-02-27 09:36:10 +0100

    ...

--- apis/api_tweet.py
+++ apis/api_tweet.py
@@ -1,6 +1,8 @@
 from bottle import post, request, response
 import x
 
+
+
 @post("/tweet")
 def _():
   try: # SUCCESS
--- x.py
+++ x.py
@@ -1,4 +1,27 @@
 from bottle import request
+import sqlite3
+import pathlib 
+
+##############################
+def dict_factory(cursor, row):
+  col_names = [col[0] for col in cursor.description]
+  return {key: value for key, value in zip(col_names, row)}
+
+##############################
+def db():
+  try:
+    db = sqlite3.connect(str(pathlib.Path(__file__).parent.resolve())+"/twitter.db") 
+    db.row_factory = dict_factory
+    return db
+  except Exception as ex:
+    print(ex)
+  finally:
+    pass
+
+
+
+
+
 
 TWEET_MIN_LEN = 1
 TWEET_MAX_LEN = 5
