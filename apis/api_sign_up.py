3960e59d448bcee5e3cf73ed0ad2174ac0990574
Santiago Donoso <SAND@kea.dk>
2023-03-13 10:46:35 +0100

    test

--- apis/api_sign_up.py
+++ apis/api_sign_up.py
@@ -5,12 +5,24 @@ import x
 def _():
     try:
         user_name = x.validate_user_name()
+        user_id = 1
+        user = {
+            "user_id" : user_id,
+            "user_name" : user_name
+        }
+        values = ""
+        for key in user:
+            values = values + f":{key},"
+        values = values.rstrip(",")
+        print(values)
+        # db.execute("INSERT INTO users VALUES(:user_id, :user_name)", user)
+        # db.execute(f"INSERT INTO users VALUES({values})", user)
         return "ok"
     except Exception as e:
         print(e)
         return {"info":str(e)} # cast to string
     finally:
-        pass
+        if "db" in locals(): db.close()
 
 
 
