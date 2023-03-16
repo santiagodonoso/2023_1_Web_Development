6265f251db08b0e12c4c8fdfa70de62ae8bf512d
Santiago Donoso <SAND@kea.dk>
2023-03-16 15:57:37 +0100

    test

--- send_sms.py
+++ send_sms.py
@@ -1,9 +1,9 @@
 import requests
 
 print("ok")
-user_api_key = ""
+user_api_key = "YOU KEY"
 sms_message = "This is great"
-sms_to_phone = ""
+sms_to_phone = "YOUR PHONE"
 
 payload = {	"user_api_key":user_api_key, 
 			"sms_message":sms_message, 
