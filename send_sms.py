6a3411214bac21e9de41d7000c60bc2a6d61c733
Santiago Donoso <SAND@kea.dk>
2023-03-16 15:57:27 +0100

    test

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,14 @@
 import requests
 
 print("ok")
-user_api_key = "cba2cd132f294ddc9e5491930f8a2d5d"
+user_api_key = ""
 sms_message = "This is great"
-sms_to_phone = "25850567"
+sms_to_phone = ""
 
 payload = {	"user_api_key":user_api_key, 
 			"sms_message":sms_message, 
 			"sms_to_phone":sms_to_phone}
+
 res = requests.post('https://smses.eu.pythonanywhere.com/api-send-sms', data=payload)
 print(res)
 print(res.text)
